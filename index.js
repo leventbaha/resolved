f77cf67bad1f1363cb6540f465c390dc163e44d9
Levent Bahadır Boran <eboran@etu.edu.tr>
2022-08-24 10:32:18 +0000

    added person.js and photo.js

--- index.js
+++ index.js
@@ -1,40 +1,19 @@
-class Person {
-  constructor(name,age) {
-    this.name = name
-    this.age = age
-    this.photos = []
-    this.likes = []
-  }
-
-  greet(person) {
-    console.log(`Hello ${person.name}, this is ${this.name}`)
-  }
-
-  addPhoto(photo) {
-    this.photos.push(photo)
-  }
-
-  likePhoto(photo) {
-    this.likes.push(photo)
-    photo.likedBy.push(this)
-  }
-}
-
-class Photo {
-  constructor(filename) {
-    this.filename = filename
-    this.likedBy = []
-   }
- }
+const Person = require("./person")
+const Photo = require("./photo")
 
 const mihri = new Person('mihri',35)
 const steve = new Person('steve',21)
 const armagan = new Person('armagan',36)
 
-const photo = new Photo('berlin.jpg')
+const berlinPhoto = new Photo('berlin.jpg')
+const munichPhoto = new Photo('munich.jpg')
 
-steve.addPhoto(photo)
-armagan.likePhoto(photo)
-mihri.likePhoto(photo)
+steve.addPhoto(berlinPhoto)
+steve.addPhoto(munichPhoto)
 
-console.log(mihri.likes[0].likedBy[0].likes[0].filename == mihri.likes[0].filename, mihri.likes[0].filename == steve.photos[0].filename)
+steve.bio = "An awesome hacker who has seen it all."
+
+armagan.likePhoto(berlinPhoto)
+mihri.likePhoto(berlinPhoto)
+
+console.log(steve.profile)
